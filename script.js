97824f6d0ea978dd96fdf120a4cfca06edac5c1f
newtreenoh <newtreenoh@gmail.com>
2023-04-12 10:46:17 +0100

    Initial back-end commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("submitButton").addEventListener("click", submitButton); //LISTENING TO THE BUTTON FOR A CLICK
+
+// CALLING THE SCRIPT ON BUTTON CLICK
+function submitButton(){
+    //CODE HERE TO CALL THE SCRIPT
+}
+  
