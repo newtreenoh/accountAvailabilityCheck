66700a48a1b2e4a618dec9ce0ba64913c719df27
newtreenoh <newtreenoh@gmail.com>
2023-04-13 10:17:14 +0100

    Requesting user input

--- accountAvailabilityCheck.py
+++ accountAvailabilityCheck.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 
+# Take user input for email
+email = input("Enter email to check availability: ")
+
 # Open Google sign-up page
 driver = webdriver.Chrome()
 driver.get("https://accounts.google.com/signup")
